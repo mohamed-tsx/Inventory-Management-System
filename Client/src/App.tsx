87e6800a35c2b7667901bb0737081066ecda5a3e
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-07 22:05:58 +0300

    Installed React and Sass for styling

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <div className="container">
+        hi <div className="second">Hh2</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
